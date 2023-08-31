7389b07935e5429cdaed89f4b4d853413d105e33
salahhusa9 <salahhusa9@gmail.com>
2023-08-31 18:25:22 +0100

    isset

--- README.md
+++ README.md
@@ -92,6 +92,9 @@ he accept all normal attributes of form tag and add some new attributes:
     </x-slot>
 </x-template-components-card>
 ```
+he accept all normal attributes of div tag and add some new attributes:
+- div-class: add class to div tag (contener div)
+- other attributes will be added to form tag
 
 ## Supported Templates
 
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,13 +1,17 @@
 @props(['header', 'footer'])
 
-<div {{ $header->attributes->class([config('template-components.defult_classes.card.div')]) }}>
+<div class="{{ $attributes->get('div-class') }} {{ config('template-components.defult_classes.card.div') }}">
+    @isset($header)
     <h5 {{ $header->attributes->class([config('template-components.defult_classes.card.header')]) }}>
         {{ $header }}
     </h5>
+    @endisset
     <div {{ $attributes->class([config('template-components.defult_classes.card.body')]) }}>
         {{ $slot }}
     </div>
-    <div {{ $footer->attributes->class([config('template-components.defult_classes.card.footer')]) }}>
-        {{ $footer }}
-    </div>
+    @isset($footer)
+        <div {{ $footer->attributes->class([config('template-components.defult_classes.card.footer')]) }}>
+            {{ $footer }}
+        </div>
+    @endisset
 </div>
